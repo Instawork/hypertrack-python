f0c95accd72829974e16878c41c24a3c79a49bec
Tapan Pandita <tapan.pandita@gmail.com>
2016-05-17 15:02:41 +0530

    Check for basestring for python 3 compatibility

--- hypertrack/resource.py
+++ hypertrack/resource.py
@@ -55,6 +55,12 @@ class HyperTrackObject(dict):
     def __repr__(self):
         ident_parts = [type(self).__name__]
 
+        # Python 3 compatibility for string types
+        try:
+            basestring
+        except NameError:
+            basestring = str
+
         if isinstance(self.get('object'), basestring):
             ident_parts.append(self.get('object'))
 
